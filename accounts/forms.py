9ee86e13a333fc1bc7bd3fac4f98d88e2d53e554
Liam Resnick <liamresnick@Liams-MacBook-Pro.local>
2025-10-18 20:31:36 -0400

    Implement User Story 1 part 1: Added login view and template

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,25 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.forms.utils import ErrorList
 from django.utils.safestring import mark_safe
+from django import forms
+from django.contrib.auth.models import User
+from .models import Profile
+
+
 class CustomErrorList(ErrorList):
     def __str__(self):
         if not self:
             return ''
         return mark_safe(''.join([f'<div class="alert alert-danger" role="alert"> {e}</div>' for e in self]))
+
 class CustomUserCreationForm(UserCreationForm):
+    latitude = forms.FloatField(required=False, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Latitude', 'id': 'id_latitude'}))
+    longitude = forms.FloatField(required=False, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'longitude', 'id': 'id_longitude'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2', 'latitude', 'longitude')
+
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__ (*args, **kwargs)
         for fieldname in ['username', 'password1', 'password2']:
@@ -14,3 +27,12 @@ class CustomUserCreationForm(UserCreationForm):
             self.fields[fieldname].widget.attrs.update(
                 {'class': 'form-control'}
             )
+    def save(self, commit=True):
+        user = super().save(commit)
+        lat = self.cleaned_data.get('latitude')
+        lon = self.cleaned_data.get('longitude')
+        if hasattr(user, 'profile'):
+            user.profile.latitude = lat
+            user.profile.longitude = lon
+            user.profile.save()
+        return user
